50c4bbb11d5dd924a554dbefd004ed2003bbe253
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-09 21:23:54 -1100

    Add form submit to script file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './style.css';
+import Utils from './modules/utils.js';
 
-const heading = document.querySelector('h1');
-heading.getAttribute('id');
+const user = new Utils();
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userName = document.querySelector('.user_name').value;
+  const userScore = document.querySelector('.user_score').value;
+
+  user.addScore(userName, userScore);
+});
