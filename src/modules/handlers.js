416a3a1d9e693f49dbeeb1df63b828431b2e9a2f
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-11 09:10:10 -1100

    Setup local storge for the id

--- src/modules/handlers.js
+++ src/modules/handlers.js
@@ -22,8 +22,16 @@ export default class FetchHelpers {
     }
   }
 
-  geID() {
+  createID() {
     const id = this.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/', { name: 'My cool new game' });
     return id.slice(15, -7).trim();
+  }
+
+  addToStorage(data) {
+    localStorage.setItem('ID', JSON.stringify(data));
+  }
+
+  getFromStorage() {
+    return JSON.parse(localStorage.getItem('ID'));
   }
 }
