a57301f1063d88e6480511900dc2d267f982fba7
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-10 13:23:26 -1100

    Add clear input function

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import Utils from './modules/utils.js';
 const user = new Utils();
 const form = document.querySelector('form');
 
+const clearInput = () => {
+  document.querySelector('.user_name').value = '';
+  document.querySelector('.user_score').value = '';
+};
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const userName = document.querySelector('.user_name').value;
   const userScore = document.querySelector('.user_score').value;
 
   user.addScore(userName, userScore);
+  clearInput();
 });
 
 window.onload = user.displayScores();
