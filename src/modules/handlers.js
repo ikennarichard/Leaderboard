cdafdc76fc6cc2c199081f64a92bb1ee85fff586
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-11 09:08:09 -1100

    Add method for generating id

--- src/modules/handlers.js
+++ src/modules/handlers.js
@@ -3,27 +3,27 @@ export default class FetchHelpers {
   async get(endpoint) {
     const resposnse = await fetch(endpoint);
     const data = await resposnse.json();
-    return data
+    return data;
   }
 
   async post(endpoint, data) {
-  try {
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    const result = response.json();
-
-
-  } catch (error) {
-    throw new Error(error);
+    try {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      const result = response.json();
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
-  }
 
-  createNewGame() {
-
+  geID() {
+    const id = this.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/', { name: 'My cool new game' });
+    return id.slice(15, -7).trim();
   }
 }
