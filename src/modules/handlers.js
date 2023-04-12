feb99553326bb19a9080f5fd7ef2ddd868f689e7
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-11 15:20:22 -1100

    Add method for creating new game

--- src/modules/handlers.js
+++ src/modules/handlers.js
@@ -1,14 +1,15 @@
 /* eslint-disable class-methods-use-this */
 import Utils from './utils.js';
+import UserScore from './userScore.js';
 
 export default class FetchHandlers {
   async get(endpoint) {
-    const resposnse = await fetch(endpoint);
-    const data = await resposnse.json();
+    const response = await fetch(endpoint);
+    const data = await response.json();
     return data;
   }
 
-  static async post(endpoint, data) {
+  async post(endpoint, data) {
     try {
       const response = await fetch(endpoint, {
         method: 'POST',
@@ -17,58 +18,18 @@ export default class FetchHandlers {
         },
         body: JSON.stringify(data),
       });
-      const result = await response.json();
-      return result;
+      return response.json();
     } catch (e) {
       throw new Error(e);
     }
-  }
-
-  static async createID() {
-    const { result } = await FetchHandlers.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', { name: 'My cool new game' });
-
-    console.log(result)
-    return result.slice(15, -7).trim();
-  }
-
-  static addToStorage(data) {
-    localStorage.setItem('ID', JSON.stringify(data));
-  }
-
-  static getFromStorage() {
-    return JSON.parse(localStorage.getItem('ID'));
   }
 
   async createNewGame() {
-    if (FetchHandlers.getFromStorage() === undefined) { 
+    if (FetchHandlers.getFromStorage()) {
       return;
     }
-    const id = await FetchHandlers.createID();
+    const { result } = await this.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', { name: 'My cool new game' });
+    const id = result.slice(15, -7);
     FetchHandlers.addToStorage(id);
-  }
-
-  async postScore() {
-    const name = document.querySelector('.user_name').value;
-    const score = document.querySelector('.user_score').value;
-    const item = {
-      name,
-      score,
-    };
-    try {
-      const res = await FetchHandlers.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${FetchHandlers.getFromStorage()}/scores`, item);
-
-      console.log(res);
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  async refreshScores() {
-    try {
-      const { result } = await this.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${FetchHandlers.getFromStorage()}/scores`);
-      Utils.displayScores(result);
-    } catch (e) {
-      throw new Error(e);
-    }
   }
 }
